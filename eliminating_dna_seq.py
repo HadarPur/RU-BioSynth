960630b3a7d569b4c0360add6e614bae424da2e4
Hadar Pur <hadar.pur@is.com>
2023-08-05 00:04:20 +0300

    continue working on the prototype, adding highlighted coding regions and starting seperate it by user's choice

--- eliminating_dna_seq.py
+++ eliminating_dna_seq.py
@@ -4,16 +4,24 @@ from dna_utils import *
 
 
 def terminal(argv):
-    s_file_path, p_file_path = handle_input_params(argv)
+    s_file_path, p_file_path = handle_initial_input_params(argv)
     seq = read_seq_from_file(s_file_path)
     # patterns = read_seq_from_file(p_file_path)
 
-    coding_regions = get_coding_region(seq)
-
     # Print the coding regions and their translations
     print(f"DNA sequence:\n{seq}\n")
-    for i, region in enumerate(coding_regions):
-        print(f"Coding region {i + 1}:\n{region}\nTranslation:\n{region.translate()}\n")
+
+    patterns = ""
+    coding_regions = get_coding_region(seq)
+
+    highlighted_sequence = highlight_coding_regions(seq, coding_regions)
+    print(f"Highlighted conding regions for the above DNA sequence:\n{highlighted_sequence}\n")
+
+    elimination_response = get_elimination_response_from_user()
+
+    if elimination_response is False:
+        coding_regions_response = get_elimination_coding_regions_from_user(coding_regions)
+        print(coding_regions_response)
 
     return
 
@@ -26,19 +34,30 @@ def gui():
 def app():
     seq = "ATGTTAGTAGTGAAGCGAATGTAAATGACCTAGTAGCCCTAGTGA"
 
+    # Print the coding regions and their translations
+    print(f"DNA sequence:\n{seq}\n")
+
     patterns = ""
     coding_regions = get_coding_region(seq)
 
-    # Print the coding regions and their translations
-    print(f"DNA sequence:\n{seq}\n")
-    for i, region in enumerate(coding_regions):
-        print(f"Coding region {i + 1}:\n{region}\nTranslation:\n{region.translate()}\n")
+    highlighted_sequence = highlight_coding_regions(seq, coding_regions)
+    print(f"Highlighted conding regions for the above DNA sequence:\n{highlighted_sequence}\n")
+
+    elimination_response = get_elimination_response_from_user()
+
+    if elimination_response is False:
+        coding_regions_response = get_elimination_coding_regions_from_user(coding_regions)
+        print(coding_regions_response)
 
     return
 
 
-if __name__ == "__main__":
-    # app()
-    terminal(sys.argv[1:])
-    # gui()
+def main():
+    if __name__ == "__main__":
+        app()
+    else:
+        terminal(sys.argv[1:])
 
+
+if __name__ == "__main__":
+    main()
