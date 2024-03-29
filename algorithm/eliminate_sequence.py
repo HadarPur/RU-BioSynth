2f265cf562208d024f913739bb3d6548280833cb
Hadar Pur <hadar.pur@is.com>
2024-03-29 21:31:44 +0300

    fix printing

--- algorithm/eliminate_sequence.py
+++ algorithm/eliminate_sequence.py
@@ -2,16 +2,25 @@ from utils.elimination_utils import EliminationUtils
 from algorithm.fsm import FSM
 from typing import Set
 from collections import defaultdict
+from settings.costs_settings import s_coding_region, w_coding_region, x_coding_region
+from settings.costs_settings import w_non_coding_region, x_non_coding_region
 
 
 class EliminateSequence:
     @staticmethod
     def eliminate(S: str, P: Set[str], C: list[dict[str, float]]) -> str:
-        print('\n' + '=' * 50)
+        print('\n' + '=' * 50 + '\n')
         print(f"Starting Elimination Process")
         print(f"Unwanted Patterns: {P}")
         print(f"Original Sequence: {S}")
-        print('=' * 50 + '\n')
+        print('\nWhen considering the costs associated with changing DNA sequences in both coding and non-coding regions, different expense structures come into play.')
+        print(f'\nCoding regions:'
+              f'\nIn coding regions, a substitution that does not change the amino acid incurs an expense of {x_coding_region}.'
+              f'\nA higher cost of {w_coding_region}is associated with substitutions that change the amino acid.'
+              f'\nA substitution that converts an amino acid to a (premature) stop codon incurs a very high cost of {s_coding_region}')
+        print(f'\nNon-Coding regions:'
+              f'\nWithin non-coding sections, altering bases within complementary pairs (A to T, T to A, C to G, G to C) incurs a cost of {x_non_coding_region}.'
+              f'\nSubstituting one base for another (e.g., A to G or T to C) raises the expense to {w_non_coding_region}.\n')
 
         sequence_length = len(S)
         backtrack = {}
@@ -56,6 +65,6 @@ class EliminateSequence:
         print("Elimination Process Completed")
         print(f"Modified Sequence: {''.join(sequence)}")
         print(f"Total Cost: {min_cost:.10g}")
-        print('=' * 50 + '\n')
+        print('\n' + '=' * 50 + '\n')
 
         return ''.join(sequence), min_cost
