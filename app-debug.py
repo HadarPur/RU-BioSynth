a20da2bc7017cc30abfd28d19d6b2ec17ad96651
Hadar Pur <hadar.pur@is.com>
2024-05-26 19:37:11 +0300

    change arch

--- app-debug.py
+++ app-debug.py
@@ -1,7 +1,7 @@
-import sys
+import time
 import unittest
+from executions.console.ide_execution import App
 
-from executions.console.terminal_execution import Terminal
 from utils.file_utils import delete_dir
 from utils.text_utils import OutputFormat, set_output_format
 
@@ -19,11 +19,11 @@ if __name__ == "__main__":
     try:
         delete_dir('report_output')
 
-        if len(sys.argv[1:]) > 0:
-            set_output_format(OutputFormat.TERMINAL)
-            Terminal(sys.argv[1:]).execute()
-        else:
-            exit("Please enter the sequence file path and unwanted file path and try again later.")
+        # DEBUG
+        execute_unittests()
+        time.sleep(0.2)
+        set_output_format(OutputFormat.TERMINAL)
+        App().execute()
 
     except KeyboardInterrupt:
         print("\nProgram stopped by the user.")
