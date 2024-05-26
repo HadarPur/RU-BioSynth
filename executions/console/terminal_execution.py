2a165330423996c77cc3eda7fba4ec41c914cc4e
Hadar Pur <hadar.pur@is.com>
2024-05-26 20:52:29 +0300

    fix message

--- executions/console/terminal_execution.py
+++ executions/console/terminal_execution.py
@@ -16,27 +16,27 @@ class Terminal:
         unwanted_patterns = PatternReader(p_file_path).read_patterns()
 
         if seq is None:
-            print("\033[91mUnfortunately, we couldn't find any sequence file. Please insert one and try again.\033[91m")
+            print("\033[91mUnfortunately, we couldn't find any sequence file. Please insert one and try again.\033[0m")
             return
 
         if len(seq) == 0:
-            print("\033[91mUnfortunately, the sequence file is empty. Please insert fully one and try again.\033[91m")
+            print("\033[91mUnfortunately, the sequence file is empty. Please insert fully one and try again.\033[0m")
             return
 
         if not is_valid_dna(seq):
-            print(f"\033[91mThe sequence:\n{seq}\n\nis not valid, please check and try again later.\033[91m")
+            print(f"\033[91mThe sequence:\n{seq}\n\nis not valid, please check and try again later.\033[0m")
             return
 
         if unwanted_patterns is None:
-            print("\033[91mUnfortunately, we couldn't find any patterns file. Please insert one and try again.\033[91m")
+            print("\033[91mUnfortunately, we couldn't find any patterns file. Please insert one and try again.\033[0m")
             return
 
         if len(seq) == 0:
-            print("\033[91mUnfortunately, the patterns file is empty. Please insert fully one and try again.\033[91m")
+            print("\033[91mUnfortunately, the patterns file is empty. Please insert fully one and try again.\033[0m")
             return
 
         if not is_valid_patterns(unwanted_patterns):
-            print(f"\033[91mThe patterns:\n{unwanted_patterns}\n\nare not valid, please check and try again later.\033[91m")
+            print(f"\033[91mThe patterns:\n{unwanted_patterns}\n\nare not valid, please check and try again later.\033[0m")
             return
 
         Shared(seq, unwanted_patterns).run()
