fix some stuff with styling and incorrect log calculation

test/test_cost_utils.py
@@ -52,7 +52,7 @@ class TestCalculateCost(unittest.TestCase):
         _, cost = calculate_cost(self.target_sequence, self.coding_positions, self.codon_usage, 8, "CTT", "A",
                                  self.alpha,
                                  self.beta, self.w)
-        self.assertAlmostEqual(cost, -np.log(self.codon_usage["TTA"]['freq']))
+        self.assertAlmostEqual(cost, -np.log10(self.codon_usage["TTA"]['freq']))
 
     @patch("utils.amino_acid_utils.AminoAcidConfig")
     def test_stop_codon_formation(self, MockAminoAcidConfig):

utils/cost_utils.py
@@ -116,7 +116,7 @@ def calculate_cost(target_sequence, coding_positions, codon_usage, i, v, sigma, 
             return changes, 0.0
         elif AminoAcidConfig.encodes_same_amino_acid(proposed_codon, target_codon):
             # Synonymous substitution with a logarithmic penalty based on codon usage
-            return changes, -np.log(codon_usage[proposed_codon]['freq'])
+            return changes, -np.log10(codon_usage[proposed_codon]['freq'])
         elif AminoAcidConfig.is_start_codon(codon_pos) or AminoAcidConfig.either_is_stop_codon(target_codon, proposed_codon):
             # Penalize stop codon formation
             return changes, float('inf')