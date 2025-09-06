035abf40f1750e909f060347e98a46be3b13d8f9
Hadar Pur <hadarpur13@gmail.com>
2025-09-06 19:23:26 +0300

    fix logo issue

--- biosynth/executions/controllers/gui_controller.py
+++ biosynth/executions/controllers/gui_controller.py
@@ -90,7 +90,7 @@ class GUIController:
         app = QApplication(sys.argv)
         ex = BaseWindow()
         ex.show()
-        icon_path = resource_path('images/BioSynth.png')
+        icon_path = resource_path('biosynth/images/BioSynth.png')
         icon = QIcon(icon_path)
         app.setWindowIcon(icon)
         app.setStyleSheet(stylesheet)
